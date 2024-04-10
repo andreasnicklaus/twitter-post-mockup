dde118403b103549ae321ad352303b77145ef7ec
Andreas Nicklaus <andreas_nicklaus@web.de>
2024-04-10 11:05:34 +0200

    Updated for Vercel Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  // adapter: vercel()
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel()
 });
